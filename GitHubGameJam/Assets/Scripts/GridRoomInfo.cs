2dbc2fef6a63a44f41b169ad70e63d8670aaa4ae
CentiMetar <mikicm321@gmail.com>
2023-11-07 18:16:34 +0100

    Pocetak mape/grida

---
+++ GitHubGameJam/Assets/Scripts/GridRoomInfo.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class GridRoomInfo 
+{
+    public int Lvl;
+    public char Type;
+    public Sprite roomIcon;
+}
